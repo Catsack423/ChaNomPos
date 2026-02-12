6fac49e63384a5c082865cb2e4a548d188ce697e
Catsack423 <face11773@gmail.com>
2026-02-12 20:21:11 +0700

    add adminstock frontend

--- resources/views/page/adminstock.blade.php
+++ resources/views/page/adminstock.blade.php
@@ -1,9 +1,26 @@
 <x-app-layout>
+    <link rel="stylesheet" href="{{ asset('css/adminstock.css') }}">
     <x-tagbaradmin />
+    <div class="grid stockcols">
+        <div class="card">
+            <h1>stock</h1>
+        </div>
 
-    <x-grid style="">
-        <x-card>
-            
-        </x-card>
-    </x-grid>
+        <div class="grid" style="gap:16px;">
+            <div class="card">
+                <h1>add</h1>
+            </div>
+
+
+        </div>
+    </div>
+    <div class="grid logcols">
+        <div class="grid" style="gap:16px;">
+            <div class="card">
+                <h1>log</h1>
+            </div>
+
+
+        </div>
+    </div>
 </x-app-layout>

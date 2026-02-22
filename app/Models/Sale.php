96372aa058a8b3f747cbcb087ac4ca6203e340af
Catsack423 <face11773@gmail.com>
2026-02-22 21:37:34 +0700

    Edit bug

--- app/Models/Sale.php
+++ app/Models/Sale.php
@@ -25,6 +25,10 @@ class Sale extends Model
         return $this->belongsTo(User::class);
     }
 
+    public function items()
+    {
+        return $this->hasMany(SaleItem::class);
+    }
 
     public function payment()
     {

9ed8a276a8537dde429a1e5775352be15490de12
nattapong-61 <nattapong.g@kkumail.com>
2026-02-27 23:50:09 +0700

    Update SaleItem.php

--- app/Models/SaleItem.php
+++ app/Models/SaleItem.php
@@ -25,6 +25,6 @@ class SaleItem extends Model
 
     public function product()
     {
-        return $this->belongsTo(Product::class);
+        return $this->belongsTo(Product::class)->withTrashed();
     }
 }
